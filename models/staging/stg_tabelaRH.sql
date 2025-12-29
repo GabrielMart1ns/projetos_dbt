22a92a33457f8116f83c78d204bffacdaf052fa9
Gabriel Martins de Souza <gabrielms12.gms@gmail.com>
2025-12-29 18:51:59 +0000

    Criação da stg_tabelaRH e int_tabelaRH_condicionais

---
+++ models/staging/stg_tabelaRH.sql
@@ -0,0 +1,25 @@
+
+SELECT
+    id_funcionario,
+    idade,
+    genero,
+    `estado civil` as estado_civil,
+    departamento,
+    funcao,
+    viagem,
+    `valor diaria` as valor_diaria,
+    indice_envolvimento_trabalho,
+    nivel_satisfacao_trabalho,
+    salario_mensal,
+    numero_empresas_anteriores,
+    disponivel_hora_extra,
+    ROUND(percentual_ultimo_aumento_salario, 2) as percentual_ultimo_aumento_salario,
+    aval_performance,
+    anos_experiencia,
+    numero_treinamentos_ano_anterior,
+    anos_na_empresa,
+    anos_funcao_atual,
+    anos_desde_ultima_promocao,
+    anos_com_gerente_atual
+FROM 
+    {{ref("tabela_rh")}}
